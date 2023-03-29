fdaee97885b486aa485f6c9bfd9482ff8047ba12
AlecRoss01 <61425095+AlecRoss01@users.noreply.github.com>
2023-03-29 14:33:25 -0400

    Update client_test.go

--- client/client_test.go
+++ client/client_test.go
@@ -8,3 +8,31 @@ func TestThirdPerson(t *testing.T) {
 		t.Errorf("thirdPerson() = %q, want %q", got, want)
 	}
 }
+
+func TestCoinFlip(t *testing.T) {
+	want := true
+	flip := coinFlip()
+	var got bool
+	if flip == "tails" || flip == "heads" {
+		got = true
+	} else {
+		got = false
+	}
+	if got != want {
+		t.Errorf("coinFlip() = %q, want heads or tails", flip)
+	}
+}
+
+func TestDiceRoll(t *testing.T) {
+	want := true
+	roll := diceRoll()
+	var got bool
+	if roll >= 1 && roll <=6 {
+		got = true
+	} else {
+		got = false
+	}
+	if got != want {
+		t.Errorf("diceRoll() = %q, want 1-6", roll)
+	}
+}
